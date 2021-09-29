5213db1a4249e8ca8d0ff6a990ba53437a839274
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 20:57:32 +0530

    Welcome message

--- src/Com/bridgelabz/queue/Queues.java
+++ src/Com/bridgelabz/queue/Queues.java
@@ -1,51 +1,6 @@
 package Com.bridgelabz.queue;
-
 public class Queues {
-    public class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-        }
-    }
-    Node front, rear;
-
-    Queues() {
-        this.front = null;
-        this.rear = null;
-    }
-
-    //Ability to create queue of 56,30 and 70
-
-    public void enQueue(int data) {
-        Node newNode = new Node(data);
-        if (this.rear == null) {
-            this.front = newNode;
-            this.rear = newNode;
-            return;
-        }
-        this.rear.next = newNode;
-        this.rear = newNode;
-        System.out.println("Element is Add: " + newNode.data);
-    }
-
-    public int size() {
-        int count = 0;
-        Node temp = this.front;
-        while (temp != null) {
-            count++;
-            temp = temp.next;
-        }
-        return count;
-    }
-
-    public void display() {
-        Node temp = front;
-        while (temp != null) {
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("Null");
+    public static void main(String[] args) {
+        System.out.println("welcome message");
     }
 }
