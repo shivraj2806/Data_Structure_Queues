413850fae62f841badea169c844eafbb4862309b
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 20:52:10 +0530

    Added: insert data in queue

---
+++ src/Com/bridgelabz/queue/Main_queue.java
@@ -0,0 +1,12 @@
+package Com.bridgelabz.queue;
+
+public class Main_queue {
+    public static void main(String[] args) {
+        Queues queue = new Queues();
+        queue.enQueue(56);
+        queue.enQueue(30);
+        queue.enQueue(70);
+        queue.display();
+    }
+    }
+
